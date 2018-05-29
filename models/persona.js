9f4f6257967553b4fab4cbc4a032c3ba7eebed69
Rodrigo Calgaro <ri.calgaro@gmail.com>
2018-05-29 19:42:12 -0300

    first commit

---
+++ models/persona.js
@@ -0,0 +1,38 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  var Persona = sequelize.define('Persona', {
+    idPersona: {
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    nombre: DataTypes.STRING(30),
+    apellido: DataTypes.STRING(30),
+    dni: DataTypes.STRING(8),
+    fecha_nacimiento: DataTypes.DATEONLY,
+    telefono: DataTypes.STRING(15),
+    direccion: DataTypes.STRING(40),
+    localidad: DataTypes.STRING(40),
+    email: DataTypes.STRING(30),
+    ocupacion: DataTypes.STRING(30),
+    }, 
+    {
+    //Para que no se agreguen los timestamp attributes (updatedAt, createdAt)
+    timestamps: false,
+    //Acá van otras configuraciones
+    indexes:[
+        //Agregando un indice 'unique' a dni
+        {
+        unique: true,
+        fields: ['dni']
+        },
+    ]
+        
+    },
+    );
+  
+  Persona.associate = function(models) {
+    //Acá se definen las asociaciones
+  };
+  return Persona;
+};
---
+++ server.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var env = require('dotenv').load();
+
+
+//For BodyParser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//Models
+var models = require("./models");
+
+//Sync Database
+
+models.sequelize.sync({
+    force: false
+}).then(function () {
+    console.log('Todo marcha bien Milhouse!')
+}).catch(function (err) {
+    console.log(err, "Algo salió mal al actualizar la Base de Datos")
+});
+ 
+ 
+app.get('/', function(req, res) {
+ 
+    res.send('Hola Mundo');
+ 
+});
+ 
+//Configurar Puerto
+app.set('port', process.env.PORT || 5000);
+
+app.listen(5000, function(err) {
+ 
+    if (!err)
+        console.log("Server started, listening at localhost:", app.get('port'));
+    else console.log(err)
+ 
+});
